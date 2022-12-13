3d14d5093e6a4f18e72ab22042adbabfdfd0cce2
Maria Eduarda Holanda <duda.mholanda@gmail.com>
2022-12-13 15:10:10 -0300

    add cache to movies

--- src/main/java/br/moviemanager/backend/controller/MovieController.java
+++ src/main/java/br/moviemanager/backend/controller/MovieController.java
@@ -3,6 +3,9 @@ package br.moviemanager.backend.controller;
 import br.moviemanager.backend.DTO.MovieDTO;
 import br.moviemanager.backend.model.Movie;
 import br.moviemanager.backend.service.MovieService;
+import br.moviemanager.backend.service.implementation.MovieServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,31 +16,37 @@ import java.util.List;
 @RequestMapping("/movie")
 public class MovieController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(MovieController.class);
     @Autowired
     private MovieService service;
 
     @PostMapping
     public Movie save(@RequestBody MovieDTO movieDTO) {
+        LOG.info("Request 'save' received to the controller!");
         return service.saveMovie(movieDTO);
     }
 
     @GetMapping
     public List<Movie> findAll(){
+        LOG.info("Request 'find_all' received to the controller!");
         return service.findAllMovie();
     }
 
     @GetMapping("{id}")
     public Movie findById(@PathVariable Long id) {
+        LOG.info("Request 'find_by_id' received to the controller!");
         return service.findMovieById(id);
     }
 
     @PutMapping("{id}")
     public ResponseEntity<Movie> update(@RequestBody MovieDTO movieDTO, @PathVariable Long id){
+        LOG.info("Request 'update' received to the controller!");
         return service.updateMovie(movieDTO, id);
     }
 
     @DeleteMapping({"/{id}"})
     public ResponseEntity<?> delete(@PathVariable long id) {
+        LOG.info("Request 'delete' received to the controller!");
         return service.deleteMovie(id);
     }
 
--- src/main/java/br/moviemanager/backend/service/implementation/MovieServiceImpl.java
+++ src/main/java/br/moviemanager/backend/service/implementation/MovieServiceImpl.java
@@ -7,9 +7,15 @@ import br.moviemanager.backend.model.MovieCastKey;
 import br.moviemanager.backend.repository.GenreRepository;
 import br.moviemanager.backend.repository.MovieRepository;
 import br.moviemanager.backend.service.MovieService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -21,6 +27,7 @@ import java.util.Set;
 @Service
 public class MovieServiceImpl implements MovieService {
 
+    private static final Logger LOG = LoggerFactory.getLogger(MovieServiceImpl.class);
     @Autowired
     private MovieRepository repository;
     @Autowired
@@ -37,23 +44,31 @@ public class MovieServiceImpl implements MovieService {
     }
 
     @Override
+    @CacheEvict(value="movies", allEntries=true)
     public Movie saveMovie(MovieDTO movieDTO) {
+        LOG.info("Adding new movie...");
         Movie movie = createMovie(movieDTO);
         return repository.save(movie);
     }
 
     @Override
+    @Cacheable(cacheNames = "movies")
     public List<Movie> findAllMovie() {
+        LOG.info("Fetching all movies...");
         return repository.findAll();
     }
 
     @Override
+    @Cacheable(cacheNames = "movies")
     public Movie findMovieById(Long movieId) {
+        LOG.info("Fetching one movie...");
         return repository.findById(movieId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
     @Override
+    @CacheEvict(value="movies", allEntries=true)
     public ResponseEntity<Movie> updateMovie(MovieDTO movieDTO, Long movieId) {
+        LOG.info("Updating movie...");
         Movie movie = createMovie(movieDTO);
         return repository.findById(movieId).map( record -> {
             record.setTitle(movie.getTitle());
@@ -66,7 +81,9 @@ public class MovieServiceImpl implements MovieService {
     }
 
     @Override
+    @CacheEvict(value="movies", allEntries=true)
     public ResponseEntity<?> deleteMovie(Long movieId) {
+        LOG.info("Deleting movie...");
         return repository.findById(movieId).map( record -> {
             repository.deleteById(movieId);
             return ResponseEntity.ok().build();
